bab20824d2adb4ce014ca3d77182ea4640aeb869
sekmo <francesco.mari@daliaresearch.com>
2018-10-19 17:45:00 +0200

    Default scope for category.questions

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   validates :name, presence: true
 
-  has_many :questions
+  has_many :questions, -> { order(created_at: :desc) }, dependent: :destroy
 
   scope :by_name, -> { order("name") }
 end
