162056c1615dfdd234f71bb115d90ddfbfb7b10e
sekmo <francesco.mari@daliaresearch.com>
2018-10-19 17:47:37 +0200

    Default scope for .questions.answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :answers, dependent: :destroy
+  has_many :answers, -> { order(created_at: :desc) }, dependent: :destroy
 
   validates :title, presence: true
 end
