d274eb22626d2796ffdbae145f45ca421e6cf835
sekmo <francesco.mari@daliaresearch.com>
2018-11-09 19:02:51 +0100

    Answers: nicier and deletable

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,6 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:update, :destroy]
-  before_action :set_question, only: [:create, :update, :destroy]
+  # before_action :set_question, only: [:update, :destroy]
 
 
   def create
@@ -23,7 +23,7 @@ class AnswersController < ApplicationController
 
   def destroy
     @answer.destroy
-    redirect_to answers_url, notice: 'Answer was successfully destroyed.'
+    redirect_to question_url(@answer.question), notice: 'Answer was successfully destroyed.'
   end
 
   private
@@ -31,9 +31,9 @@ class AnswersController < ApplicationController
       @answer = Answer.find(params[:id])
     end
 
-    def set_question
-      @question = Question.find(params[:answer][:question_id])
-    end
+    # def set_question
+    #   @question = Question.find(params[:question_id])
+    # end
 
     def answer_params
       params.require(:answer).permit(:body, :question_id, :user_id)
