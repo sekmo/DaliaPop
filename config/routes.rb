bfecdc12a435b54414f59eeab274c49efb618876
sekmo <francesco.mari@daliaresearch.com>
2018-11-13 14:58:03 +0100

    User inserts also first name and last name in the registration

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   root 'pages#show', page: 'welcome'
   get  'pages/:page', to: 'pages#show', as: 'pages'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 end
